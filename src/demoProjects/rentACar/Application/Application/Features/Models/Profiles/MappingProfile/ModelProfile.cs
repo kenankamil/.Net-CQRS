f35a5f116f6f4347fa350fc61a438ce32464f3e2
EXT02D47162 <ozge.mutlu@consultant.turkcell.com.tr>
2022-09-09 14:45:54 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/Application/Features/Models/Profiles/MappingProfile/ModelProfile.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Models.Dtos;
+using Application.Features.Models.Models;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles.MappingProfile
+{
+    public class ModelProfile : Profile
+    {
+        public ModelProfile()
+        {
+            CreateMap<Model, ModelListDto>().ForMember(m => m.BrandName, opt => opt.MapFrom(m => m.Brand.Name)).ReverseMap();
+            CreateMap<IPaginate<Model>, ModelListModel>().ReverseMap();
+        }
+    }
+}
